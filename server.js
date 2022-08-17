41bc4cc1430667027a2b66bd00fbdc9f740fa7b4
fredkk22 <freddykwak123@gmail.com>
2022-08-17 15:15:08 -0400

    Added the server.js file and installed the npm
    Tested the localhost with PORT 3001, and it worked completely fine. I also added a helpers folder, which generates a random ID and contains the fs functions for reading, writing, and reading/appending files

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const { readFromFile, writeToFile, readAndAppend } = require('./helpers/fsUtils');
+const uuid = require('./helpers/uuid');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for all your notes`);
+
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+
+app.post('/api/notes', (req, res) => {
+    console.info(`${req.method} request received for all your notes`);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid()
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`New Note Added! 🚀`);
+    } else {
+        res.error("Error! Couldn't add note")
+    }
+});
+
+app.listen(PORT, () =>
+    console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
